6dac4bbcf5ad3718340b52d6510261a0efe348c9
Ben Whitney <ben.whitn@gmail.com>
2020-01-29 23:53:09 +0000

    sdfghj

--- src/Faq.js
+++ src/Faq.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 // import styled, { keyframes } from "styled-components";
 // import { fadeInDown } from "react-animations";
 import { CSSTransitionGroup } from "react-transition-group";
@@ -26,8 +26,6 @@ import { CSSTransitionGroup } from "react-transition-group";
 const Faq = () => {
   const [activeAnswers, setActiveAnswers] = useState([]);
 
-  // const questionOneRef = useRef(null);
-
   const handleActiveAnswer = currentAnswer => {
     console.log(activeAnswers);
     if (activeAnswers.includes(currentAnswer)) {
@@ -41,10 +39,11 @@ const Faq = () => {
     }
   };
 
-  // const handleClick = () => {
-  //   console.log("its working");
-  //   questionOneRef.current.classList.toggle("active");
-  // };
+  const answer1 = useRef(null);
+
+  const handleClick = () => {
+    answer1.current.classList.toggle("active");
+  };
 
   return (
     <section class="faq-container">
@@ -55,76 +54,91 @@ const Faq = () => {
           answered please feel free to email us.
         </p>
       </div>
-
       <div class="faq-container-menu">
         <hr />
-        <div class="question" onClick={() => handleActiveAnswer("answer1")}>
+        <div class="question" onClick={handleClick}>
           <p>What is Bookmark?</p>
           <img src={require("./images/icon-arrow.svg")} alt="" />
         </div>
-        <CSSTransitionGroup
+        {/* <CSSTransitionGroup
           transitionName="example"
-          transitionEnterTimeout={1}
-          transitionLeaveTimeout={1}
-        >
-          {activeAnswers.includes("answer1") ? (
-            <div key={123} class="answer">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-                tincidunt justo eget ultricies fringilla. Phasellus blandit
-                ipsum quis quam ornare mattis.
-              </p>
-            </div>
-          ) : null}
-        </CSSTransitionGroup>
+          transitionEnterTimeout={300}
+          transitionLeaveTimeout={300}
+        > */}
+        <div class="answer" ref={answer1}>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+            tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum
+            quis quam ornare mattis.
+          </p>
+        </div>
+        {/* </CSSTransitionGroup> */}
         <hr />{" "}
         <div class="question" onClick={() => handleActiveAnswer("answer2")}>
           <p>How can I request a new browser?</p>
           <img src={require("./images/icon-arrow.svg")} alt="" />
         </div>
-        {activeAnswers.includes("answer2") ? (
-          <div class="answer">
-            <p>
-              Vivamus luctus eros aliquet convallis ultricies. Mauris augue
-              massa, ultricies non ligula. Suspendisse imperdiet. Vivamus luctus
-              eros aliquet convallis ultricies. Mauris augue massa, ultricies
-              non ligula. Suspendisse imperdie tVivamus luctus eros aliquet
-              convallis ultricies. Mauris augue massa, ultricies non ligula.
-              Suspendisse imperdiet.
-            </p>
-          </div>
-        ) : null}
+        <CSSTransitionGroup
+          transitionName="example"
+          transitionEnterTimeout={300}
+          transitionLeaveTimeout={300}
+        >
+          {activeAnswers.includes("answer2") ? (
+            <div class="answer">
+              <p>
+                Vivamus luctus eros aliquet convallis ultricies. Mauris augue
+                massa, ultricies non ligula. Suspendisse imperdiet. Vivamus
+                luctus eros aliquet convallis ultricies. Mauris augue massa,
+                ultricies non ligula. Suspendisse imperdie tVivamus luctus eros
+                aliquet convallis ultricies. Mauris augue massa, ultricies non
+                ligula. Suspendisse imperdiet.
+              </p>
+            </div>
+          ) : null}
+        </CSSTransitionGroup>
         <hr />{" "}
         <div class="question" onClick={() => handleActiveAnswer("answer3")}>
           <p>Is there a mobile app?</p>
           <img src={require("./images/icon-arrow.svg")} alt="" />
         </div>
-        {activeAnswers.includes("answer3") ? (
-          <div class="answer">
-            <p>
-              Sed consectetur quam id neque fermentum accumsan. Praesent luctus
-              vestibulum dolor, ut condimentum urna vulputate eget. Cras in
-              ligula quis est pharetra mattis sit amet pharetra purus. Sed
-              sollicitudin ex et ultricies bibendum.
-            </p>
-          </div>
-        ) : null}
+        <CSSTransitionGroup
+          transitionName="example"
+          transitionEnterTimeout={300}
+          transitionLeaveTimeout={300}
+        >
+          {activeAnswers.includes("answer3") ? (
+            <div class="answer">
+              <p>
+                Sed consectetur quam id neque fermentum accumsan. Praesent
+                luctus vestibulum dolor, ut condimentum urna vulputate eget.
+                Cras in ligula quis est pharetra mattis sit amet pharetra purus.
+                Sed sollicitudin ex et ultricies bibendum.
+              </p>
+            </div>
+          ) : null}
+        </CSSTransitionGroup>
         <hr />{" "}
         <div class="question" onClick={() => handleActiveAnswer("answer4")}>
           <p>What about other Chromium browsers?</p>
           <img src={require("./images/icon-arrow.svg")} alt="" />
         </div>
-        {activeAnswers.includes("answer4") ? (
-          <div class="answer">
-            <p>
-              Integer condimentum ipsum id imperdiet finibus. Vivamus in
-              placerat mi, at euismod dui. Aliquam vitae neque eget nisl gravida
-              pellentesque non ut velit. More Info 35,000+ already joined Stay
-              up-to-date with what we’re doing Contact Us Features Pricing
-              Contact
-            </p>
-          </div>
-        ) : null}
+        <CSSTransitionGroup
+          transitionName="example"
+          transitionEnterTimeout={300}
+          transitionLeaveTimeout={300}
+        >
+          {activeAnswers.includes("answer4") ? (
+            <div class="answer">
+              <p>
+                Integer condimentum ipsum id imperdiet finibus. Vivamus in
+                placerat mi, at euismod dui. Aliquam vitae neque eget nisl
+                gravida pellentesque non ut velit. More Info 35,000+ already
+                joined Stay up-to-date with what we’re doing Contact Us Features
+                Pricing Contact
+              </p>
+            </div>
+          ) : null}
+        </CSSTransitionGroup>
         <hr />
       </div>
       <button>More Info</button>
