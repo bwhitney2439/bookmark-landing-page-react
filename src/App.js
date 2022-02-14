ad87b6d6c425fb9463b466508addec1cb447107e
Ben Whitney <ben.whitn@gmail.com>
2022-02-14 18:40:05 +0000

    asdsadsa

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
@@ -14,6 +14,7 @@ const App = () => {
 
   return (
     <React.Fragment>
+      <div>{data}</div>;
       <Header />
       <Main />
       <Footer />
